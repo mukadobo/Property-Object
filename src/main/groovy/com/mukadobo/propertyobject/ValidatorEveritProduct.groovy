64833315f8921f7f78b0ebae2af632f6d8fc71b9
Stevel Lintz <Stevel.Lintz@wolterskluwer.com>
2018-07-01 13:51:32 -0400

    Tutorial example ~ Everit validation (type: product)

---
+++ src/main/groovy/com/mukadobo/propertyobject/ValidatorEveritProduct.groovy
@@ -0,0 +1,35 @@
+package com.mukadobo.propertyobject
+
+import groovy.json.JsonBuilder
+import org.everit.json.schema.Schema
+import org.everit.json.schema.loader.SchemaLoader
+import org.json.JSONObject
+import org.json.JSONTokener
+
+class ValidatorEveritProduct
+{
+	static void main (String[] args)
+	{
+		String productJsonText = '''\
+		{
+		  "productId": 1,
+		  "productName": "A green door",
+		  "price": 12.50,
+		  "tags": [ "home", "green" ]
+		}
+		'''.stripIndent()
+		getClass().getResourceAsStream("/example-product+schema.json").withCloseable {
+			JSONObject rawSchema = new JSONObject(new JSONTokener(it))
+			Schema schema = SchemaLoader.load(rawSchema)
+
+			println "productJsonText: $productJsonText"
+			println ""
+			println "rawSchema: ${new JsonBuilder(rawSchema.toMap()).toPrettyString()})"
+			println ""
+			println "schema: $schema"
+			println ""
+
+			schema.validate(new JSONObject(productJsonText)) // throws a ValidationException if this object is invalid
+		}
+	}
+}
