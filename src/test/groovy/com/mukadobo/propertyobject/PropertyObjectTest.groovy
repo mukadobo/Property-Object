c95e962663cf5d8f8dc6009d55040be1120acd5d
Stevel Lintz <Stevel.Lintz@wolterskluwer.com>
2018-07-04 12:17:49 -0400

    Reformatting sweetness

--- src/test/groovy/com/mukadobo/propertyobject/PropertyObjectTest.groovy
+++ src/test/groovy/com/mukadobo/propertyobject/PropertyObjectTest.groovy
@@ -7,71 +7,81 @@ class PropertyObjectTest extends Specification
 	def "YAML to JSON - all value primitives"()
 	{
 		expect:
-		PropertyObject.fromYaml(yamlText).toJson() == jsonText
+
+			PropertyObject.fromYaml(yamlText).toJson() == jsonText
 
 		where:
-		yamlText  | jsonText
-		'a: A'    | '{"a" : "A"}'
-		'a: 0'    | '{"a" : 0}'
-		'a: true' | '{"a" : true}'
-		'a: 2.7'  | '{"a" : 2.7}'
-		'a: null' | '{"a" : null}'
+
+			yamlText  | jsonText
+			'a: A'    | '{"a" : "A"}'
+			'a: 0'    | '{"a" : 0}'
+			'a: true' | '{"a" : true}'
+			'a: 2.7'  | '{"a" : 2.7}'
+			'a: null' | '{"a" : null}'
 	}
 
 	def "YAML to JSON - Simple Lists"()
 	{
 		expect:
-		PropertyObject.fromYaml(yamlText).toJson() == jsonText
+
+			PropertyObject.fromYaml(yamlText).toJson() == jsonText
 
 		where:
-		yamlText     | jsonText
-		'[]'         | '[]'
-		'["a"]'      | '["a"]'
-		'[1]'        | '[1]'
-		'[null]'     | '[null]'
-		'[k: null]'  | '[{"k" : null}]'
-		'[0,1,2,3]'  | '[0,1,2,3]'
+
+			yamlText     | jsonText
+			'[]'         | '[]'
+			'["a"]'      | '["a"]'
+			'[1]'        | '[1]'
+			'[null]'     | '[null]'
+			'[k: null]'  | '[{"k" : null}]'
+			'[0,1,2,3]'  | '[0,1,2,3]'
 	}
 
 	def "YAML to JSON - Nested Lists"()
 	{
 		expect:
-		PropertyObject.fromYaml(yamlText).toJson() == jsonText
+
+			PropertyObject.fromYaml(yamlText).toJson() == jsonText
 
 		where:
-		yamlText         | jsonText
-		'[[]]'           | '[[]]'
-		'[[0]]'          | '[[0]]'
-		'[[0,1]]'        | '[[0,1]]'
-		'[[0,1],[],[2]]' | '[[0,1],[],[2]]'
+
+			yamlText         | jsonText
+			'[[]]'           | '[[]]'
+			'[[0]]'          | '[[0]]'
+			'[[0,1]]'        | '[[0,1]]'
+			'[[0,1],[],[2]]' | '[[0,1],[],[2]]'
 	}
 
 	def "YAML to JSON - Simple Maps"()
 	{
 		expect:
-		PropertyObject.fromYaml(yamlText).toJson() == jsonText
+
+			PropertyObject.fromYaml(yamlText).toJson() == jsonText
 
 		where:
-		yamlText     | jsonText
-		'{}'         | '{}'
-		'{a}'        | '{"a" : null}'
-		'{a: }'      | '{"a" : null}'
-		'{a: null}'  | '{"a" : null}'
-		'{a: "A"}'   | '{"a" : "A"}'
-		'{a: 0}'     | '{"a" : 0}'
-		'{a: 2.7}'   | '{"a" : 2.7}'
+
+			yamlText     | jsonText
+			'{}'         | '{}'
+			'{a}'        | '{"a" : null}'
+			'{a: }'      | '{"a" : null}'
+			'{a: null}'  | '{"a" : null}'
+			'{a: "A"}'   | '{"a" : "A"}'
+			'{a: 0}'     | '{"a" : 0}'
+			'{a: 2.7}'   | '{"a" : 2.7}'
 	}
 
 	def "YAML to JSON - Nested Maps"()
 	{
 		expect:
-		PropertyObject.fromYaml(yamlText).toJson() == jsonText
+
+			PropertyObject.fromYaml(yamlText).toJson() == jsonText
 
 		where:
-		yamlText                       | jsonText
-		'{a0: {}}'                     | '{"a0" : {}}'
-		'{a0: {}, b0:{}}'              | '{"a0" : {}, "b0" : {}}'
-		'{a0: {a1: A1}, b0:{b1: B1}}'  | '{"a0" : {"a1" : "A1"}, "b0" : {"b1" : "B1"}}'
+
+			yamlText                       | jsonText
+			'{a0: {}}'                     | '{"a0" : {}}'
+			'{a0: {}, b0:{}}'              | '{"a0" : {}, "b0" : {}}'
+			'{a0: {a1: A1}, b0:{b1: B1}}'  | '{"a0" : {"a1" : "A1"}, "b0" : {"b1" : "B1"}}'
 	}
 
 }
