054b5fd74bb8beb9e46061a27e69dd87790b8a33
Stevel Lintz <Stevel.Lintz@wolterskluwer.com>
2018-07-04 05:18:08 -0400

    Add "URL-sewing" methods: toUrlText(...) and toUrl(...)

--- src/main/groovy/com/mukadobo/json/schema/JsonSchemaUrl.groovy
+++ src/main/groovy/com/mukadobo/json/schema/JsonSchemaUrl.groovy
@@ -2,6 +2,8 @@ package com.mukadobo.json.schema
 
 class JsonSchemaUrl
 {
+	static final SCHEME = "jsonschema"
+
 	/**
 	 * Modify the system property {@code java.protocol.handler.pkgs} to include
 	 * the package which provides {@code **.jsonschema.Handler}.
@@ -16,5 +18,31 @@ class JsonSchemaUrl
 		System.setProperty('java.protocol.handler.pkgs', jphpPlus)
 
 		jphpGiven
+	}
+
+	/**
+	 *
+	 */
+	static String toUrlText(Class classRef, Boolean canonical = true, String prefix = "jsonschema")
+	{
+		String classPathPart = (canonical
+				? classRef.getName()
+				: classRef.getName().replaceFirst(/^.*[.]/, "")
+			)
+			.replaceAll(/[.]/, "/")
+			.replaceAll(/[$]/, ".")
+
+
+		String path = "${(!canonical && prefix) ? (prefix + '/') : ''}${classPathPart}-schema.json"
+			.replaceFirst('^/+', "")
+
+		"$SCHEME:$path"
+	}
+
+	static URL toUrl(Class classRef, Boolean canonical = true, String prefix = "jsonschema")
+	{
+		String urlText = toUrlText(classRef, canonical, prefix)
+
+		new URL(urlText)
 	}
 }
