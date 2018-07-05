c8f246570b6d2fc84515a986e3d7036bcfc1f6b5
Stevel Lintz <Stevel.Lintz@wolterskluwer.com>
2018-07-05 10:39:00 -0400

    Recast: registerHandlerPackage() into static init block

--- src/main/groovy/com/mukadobo/json/schema/JsonSchemaUrl.groovy
+++ src/main/groovy/com/mukadobo/json/schema/JsonSchemaUrl.groovy
@@ -16,19 +16,15 @@ class JsonSchemaUrl
 	/**
 	 * Modify the system property {@code java.protocol.handler.pkgs} to include
 	 * the package which provides {@code **.jsonschema.Handler}.
-	 *
-	 * @return The original value of the property, which may be null.
 	 */
-	static String registerHandlerPackage()
+	static
 	{
 		String jphpGiven = System.getProperty('java.protocol.handler.pkgs')
 		String jphpPlus  = (jphpGiven ? "$jphpGiven|" : "") + 'com.mukadobo.json.schema'
-
+		
 		System.setProperty('java.protocol.handler.pkgs', jphpPlus)
-
-		jphpGiven
 	}
-
+	
 	/**
 	 *
 	 */
