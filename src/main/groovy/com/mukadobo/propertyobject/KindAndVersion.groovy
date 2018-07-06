4a6aa559d5a3a1afd77cc0971bab6e6b1960aadc
Stevel Lintz <Stevel.Lintz@wolterskluwer.com>
2018-07-05 23:14:50 -0400

    Add: KindAndVersion.Base

--- src/main/groovy/com/mukadobo/propertyobject/KindAndVersion.groovy
+++ src/main/groovy/com/mukadobo/propertyobject/KindAndVersion.groovy
@@ -1,9 +1,37 @@
 package com.mukadobo.propertyobject
 
 import com.mukadobo.version.VersionChain
+import org.json.JSONObject
 
 interface KindAndVersion
 {
 	String       getKind()
 	VersionChain getVersion()
+	
+	class Base implements KindAndVersion
+	{
+		private final String       kind
+		private final VersionChain version
+		
+		protected Base(String kind, VersionChain version)
+		{
+			this.kind    = kind
+			this.version = version
+		}
+		
+		protected Base(JSONObject jsonDom)
+		{
+			this.kind    = jsonDom.kind
+			this.version = new VersionChain(jsonDom.version)
+		}
+		
+		@Override String       getKind()    { kind }
+		@Override VersionChain getVersion() { version }
+		
+		@Override
+		String toString()
+		{
+			"$kind:$version"
+		}
+	}
 }
