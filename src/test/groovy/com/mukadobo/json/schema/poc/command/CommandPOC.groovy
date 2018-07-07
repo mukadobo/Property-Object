07d7a10c0f4f76e078fab0577924e29f79666342
Stevel Lintz <Stevel.Lintz@wolterskluwer.com>
2018-07-07 11:23:53 -0400

    Split off: CommandTest from CommandPOC

--- src/test/groovy/com/mukadobo/json/schema/poc/command/CommandPOC.groovy
+++ src/test/groovy/com/mukadobo/json/schema/poc/command/CommandPOC.groovy
@@ -1,8 +1,6 @@
 package com.mukadobo.json.schema.poc.command
 
-import com.mukadobo.json.schema.JsonSchema
 import com.mukadobo.propertyobject.KindAndVersion.Base
-import groovy.json.JsonOutput
 import org.apache.commons.io.IOUtils
 import org.apache.http.HttpEntity
 import org.apache.http.NameValuePair
@@ -17,139 +15,13 @@ import org.apache.http.message.BasicNameValuePair
 import org.apache.http.util.EntityUtils
 import org.apache.log4j.Logger
 import org.json.JSONObject
-import org.json.JSONTokener
 import spock.lang.Specification
 import spock.lang.Unroll
 
 class CommandPOC extends Specification
 {
 	static private Logger                  logger        = Logger.getLogger(CommandPOC.class)
-	static private Map<String, String>     sampleText    = new LinkedHashMap<>()
-	static private Map<String, JSONObject> sampleJsonDom = new LinkedHashMap<>()
-	
-	static private String getSampleText(String name)
-	{
-		if (sampleText.get(name) == null)
-		{
-			String path = "samples/$name"
-			
-			InputStream stream = CommandPOC.class.getResourceAsStream(path)
-			if (!stream)
-			{
-				throw new RuntimeException("can't open resource-stream for: $name")
-			}
-			
-			stream.withCloseable {
-				String text = IOUtils.toString(it, "UTF-8")
-				
-				sampleText.put(name, text)
-			}
-		}
-		
-		sampleText.get(name)
-	}
-	
-	static private JSONObject getSampleJsonDom(String name)
-	{
-		if (sampleJsonDom.get(name) == null)
-		{
-			String     text    = getSampleText(name)
-			JSONObject jsonDom = new JSONObject(new JSONTokener(text))
-			
-			sampleJsonDom.put(name, jsonDom)
-		}
-		
-		sampleJsonDom.get(name)
-	}
-	
-	@Unroll
-	def "JSON validation ~ #pogoClass.getSimpleName()"()
-	{
-		expect:
-			
-			def schema = new JsonSchema(pogoClass)
-			schema.validate(jsonText) == null
 
-		where:
-			
-			pogoClass       | jsonText
-			Subject  .class | getSampleJsonDom("simple/subject.json")
-			Predicate.class | getSampleJsonDom("simple/predicate.json")
-			Command  .class | getSampleJsonDom("simple/command.json")
-	}
-	
-	def "POGO from JSON ~ Subject"()
-	{
-		when:
-			
-			Subject probject = new Subject(getSampleJsonDom("simple/subject.json"))
-		
-		then:
-			
-			probject.kind == "Subject"
-	}
-	
-	def "POGO from JSON ~ Predicate"()
-	{
-		when:
-			
-			Predicate probject = new Predicate(getSampleJsonDom("simple/predicate.json"))
-		
-		then:
-			
-			probject.kind == "Predicate"
-	}
-	
-	def "POGO from JSON ~ Command"()
-	{
-		when:
-			
-			Command probject = new Command(getSampleJsonDom("simple/command.json"))
-		
-		then:
-			
-			probject.kind == "Command"
-	}
-	
-	@Unroll
-	def "JSON round-trip ~ #refClass.getSimpleName()"()
-	{
-		when:
-		
-			JSONObject firstDom  = new JSONObject(new JSONTokener(refText))
-			Base       firstObj  = refClass.newInstance(firstDom)
-		
-		then:
-			
-			refClass.getSimpleName() == firstObj.getKind()
-			
-		when:
-			
-			String     dupeText  = JsonOutput.toJson(firstObj)
-			JSONObject dupeDom   = new JSONObject(new JSONTokener(dupeText))
-			
-		then:
-		
-			def schema = new JsonSchema(refClass)
-			schema.validate(dupeDom) == null
-		
-		when:
-			
-			Base dupeObj = refClass.newInstance(dupeDom)
-		
-		then:
-			
-			firstObj == dupeObj
-			JsonOutput.toJson(firstObj) == JsonOutput.toJson(dupeObj)
-			
-		where:
-			
-			refClass        | refText
-			Predicate.class | getSampleText('simple/predicate.json')
-			Subject  .class | getSampleText('simple/subject.json')
-			Command  .class | getSampleText('simple/command.json')
-	}
-	
 	def "GET a web page ~ Apache quick start ~ HttpClient"()
 	{
 		// initially from: https://hc.apache.org/httpcomponents-client-ga/quickstart.html
@@ -230,4 +102,28 @@ class CommandPOC extends Specification
 		
 			logger.debug("content2 = ${content2}")
 	}
+	
+	def "Command ~ #sampleName [UNROLL]"() { expect: true }
+	
+	@Unroll
+	def "Command ~ #sampleName"()
+	{
+		// curl -X GET "https://api-v3.mbta.com/stops/8553?api_key=2f1ac323439541c194f027bad0e59f5c" -H "accept: application/vnd.api+json"
+		// curl -X GET "https://api-v3.mbta.com/stops/8553" -H "accept: application/vnd.api+json" -H "x-api-key: 2f1ac323439541c194f027bad0e59f5c"
+		
+		when:
+			
+			JSONObject commandDom  = CommandTest.getSampleJsonDom("${sampleName}.json")
+			Base       command     = refClass.newInstance(commandDom)
+		
+		then:
+			
+			refClass.getSimpleName() == command.getKind()
+		
+		where:
+			
+			refClass        | sampleName
+			Command  .class | "HttpRest/mbta-list"
+	}
+	
 }
---
+++ src/test/groovy/com/mukadobo/json/schema/poc/command/CommandTest.groovy
@@ -0,0 +1,141 @@
+package com.mukadobo.json.schema.poc.command
+
+import com.mukadobo.json.schema.JsonSchema
+import com.mukadobo.propertyobject.KindAndVersion.Base
+import groovy.json.JsonOutput
+import org.apache.commons.io.IOUtils
+import org.json.JSONObject
+import org.json.JSONTokener
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class CommandTest extends Specification
+{
+	static private Map<String, String>     sampleText    = new LinkedHashMap<>()
+	static private Map<String, JSONObject> sampleJsonDom = new LinkedHashMap<>()
+	
+	static String getSampleText(String name)
+	{
+		if (sampleText.get(name) == null) {
+			String path = "samples/$name"
+			
+			InputStream stream = CommandTest.class.getResourceAsStream(path)
+			if (!stream) {
+				throw new RuntimeException("can't open resource-stream for: $path")
+			}
+			
+			stream.withCloseable {
+				String text = IOUtils.toString(it, "UTF-8")
+				
+				sampleText.put(name, text)
+			}
+		}
+		
+		sampleText.get(name)
+	}
+	
+	static JSONObject getSampleJsonDom(String name)
+	{
+		if (sampleJsonDom.get(name) == null) {
+			String text = getSampleText(name)
+			JSONObject jsonDom = new JSONObject(new JSONTokener(text))
+			
+			sampleJsonDom.put(name, jsonDom)
+		}
+		
+		sampleJsonDom.get(name)
+	}
+	
+	def "JSON validation ~ #pogoClass.getSimpleName() [UNROLL]"() { expect: true }
+	
+	@Unroll
+	def "JSON validation ~ #pogoClass.getSimpleName()"()
+	{
+		expect:
+			
+			def schema = new JsonSchema(pogoClass)
+			schema.validate(jsonText) == null
+		
+		where:
+			
+			pogoClass       | jsonText
+			Subject.class   | getSampleJsonDom("simple/subject.json")
+			Predicate.class | getSampleJsonDom("simple/predicate.json")
+			Command.class   | getSampleJsonDom("simple/command.json")
+	}
+	
+	def "POGO from JSON ~ Subject"()
+	{
+		when:
+			
+			Subject probject = new Subject(getSampleJsonDom("simple/subject.json"))
+		
+		then:
+			
+			probject.kind == "Subject"
+	}
+	
+	def "POGO from JSON ~ Predicate"()
+	{
+		when:
+			
+			Predicate probject = new Predicate(getSampleJsonDom("simple/predicate.json"))
+		
+		then:
+			
+			probject.kind == "Predicate"
+	}
+	
+	def "POGO from JSON ~ Command"()
+	{
+		when:
+			
+			Command probject = new Command(getSampleJsonDom("simple/command.json"))
+		
+		then:
+			
+			probject.kind == "Command"
+	}
+	
+	def "JSON round-trip ~ #refClass.getSimpleName() [UNROLL]"() { expect: true }
+	
+	@Unroll
+	def "JSON round-trip ~ #refClass.getSimpleName()"()
+	{
+		when:
+			
+			JSONObject firstDom = new JSONObject(new JSONTokener(refText))
+			Base firstObj = refClass.newInstance(firstDom)
+		
+		then:
+			
+			refClass.getSimpleName() == firstObj.getKind()
+		
+		when:
+			
+			String dupeText = JsonOutput.toJson(firstObj)
+			JSONObject dupeDom = new JSONObject(new JSONTokener(dupeText))
+		
+		then:
+			
+			def schema = new JsonSchema(refClass)
+			schema.validate(dupeDom) == null
+		
+		when:
+			
+			Base dupeObj = refClass.newInstance(dupeDom)
+		
+		then:
+			
+			firstObj == dupeObj
+			JsonOutput.toJson(firstObj) == JsonOutput.toJson(dupeObj)
+		
+		where:
+			
+			refClass        | refText
+			Predicate.class | getSampleText('simple/predicate.json')
+			Subject.class   | getSampleText('simple/subject.json')
+			Command.class   | getSampleText('simple/command.json')
+	}
+	
+}
