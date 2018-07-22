0fc96319f72771ca039a2b91134649b886813392
Stevel Lintz <Stevel.Lintz@wolterskluwer.com>
2018-07-21 22:57:24 -0400

    Proper DockerVerb enum's "supported" flag; Added: JavaDoc ~ DockerVerb

--- src/test/groovy/com/mukadobo/json/schema/poc/command/DockerVerb.groovy
+++ src/test/groovy/com/mukadobo/json/schema/poc/command/DockerVerb.groovy
@@ -1,24 +1,35 @@
 package com.mukadobo.json.schema.poc.command
 
+/**
+ * An {@code enum} that represents all the docker "child" commands, each with a flag indicating
+ * if that command is supported. As of 2018-07-21, all "experimental" features are NOT supported.
+ * Also, all Docker Swarm related features, plus a few that are only sensible from an interactive
+ * terminal, are marked NOT supported.
+ * <BR><BR>
+ * Note that merely being marked here as supported doesn't meant that the required {@link DockerActor}
+ * sub-class is available. It is expected that any DockerActor factory will fail gracefully when
+ * it can't find an appropriate sub-class for a given DockerVerb. (Gracefully, as in helpful
+ * error feedback, vs torturous stack traces).
+ */
 enum DockerVerb
 {
 	// list taken from https://docs.docker.com/engine/reference/commandline/docker/#child-commands
 	
 	ATTACH    (false),  // Attach local standard input, output, and error streams to a running container
 	BUILD     (true ),  // Build an image from a Dockerfile
-	CHECKPOINT(true ),  // Manage checkpoints
+	CHECKPOINT(false),  // Manage checkpoints
 	COMMIT    (true ),  // Create a new image from a container’s changes
-	CONFIG    (true ),  // Manage Docker configs
-	CONTAINER (true ),  // Manage containers
+	CONFIG    (false),  // Manage Docker configs
+	CONTAINER (false),  // Manage containers
 	CP        (true ),  // Copy files/folders between a container and the local filesystem
 	CREATE    (true ),  // Create a new container
-	DEPLOY    (true ),  // Deploy a new stack or update an existing stack
+	DEPLOY    (false),  // Deploy a new stack or update an existing stack
 	DIFF      (true ),  // Inspect changes to files or directories on a container’s filesystem
 	EVENTS    (true ),  // Get real time events from the server
 	EXEC      (true ),  // Run a command in a running container
 	EXPORT    (true ),  // Export a container’s filesystem as a tar archive
 	HISTORY   (true ),  // Show the history of an image
-	IMAGE     (true ),  // Manage images
+	IMAGE     (false),  // Manage images
 	IMAGES    (true ),  // List images
 	IMPORT    (true ),  // Import the contents from a tarball to create a filesystem image
 	INFO      (true ),  // Display system-wide information
@@ -28,11 +39,11 @@ enum DockerVerb
 	LOGIN     (true ),  // Log in to a Docker registry
 	LOGOUT    (true ),  // Log out from a Docker registry
 	LOGS      (true ),  // Fetch the logs of a container
-	MANIFEST  (true ),  // Manage Docker image manifests and manifest lists
+	MANIFEST  (false),  // Manage Docker image manifests and manifest lists
 	NETWORK   (true ),  // Manage networks
-	NODE      (true ),  // Manage Swarm nodes
+	NODE      (false),  // Manage Swarm nodes
 	PAUSE     (true ),  // Pause all processes within one or more containers
-	PLUGIN    (true ),  // Manage plugins
+	PLUGIN    (false),  // Manage plugins
 	PORT      (true ),  // List port mappings or a specific mapping for the container
 	PS        (true ),  // List containers
 	PULL      (true ),  // Pull an image or a repository from a registry
@@ -43,14 +54,14 @@ enum DockerVerb
 	RMI       (true ),  // Remove one or more images
 	RUN       (true ),  // Run a command in a new container
 	SAVE      (true ),  // Save one or more images to a tar archive (streamed to STDOUT by default)
-	SEARCH    (true ),  // Search the Docker Hub for images
-	SECRET    (true ),  // Manage Docker secrets
-	SERVICE   (true ),  // Manage services
-	STACK     (true ),  // Manage Docker stacks
+	SEARCH    (false),  // Search the Docker Hub for images
+	SECRET    (false),  // Manage Docker secrets
+	SERVICE   (false),  // Manage services
+	STACK     (false),  // Manage Docker stacks
 	START     (true ),  // Start one or more stopped containers
 	STATS     (true ),  // Display a live stream of container(s) resource usage statistics
 	STOP      (true ),  // Stop one or more running containers
-	SWARM     (true ),  // Manage Swarm
+	SWARM     (false),  // Manage Swarm
 	SYSTEM    (true ),  // Manage Docker
 	TAG       (true ),  // Create a tag TARGET_IMAGE that refers to SOURCE_IMAGE
 	TOP       (true ),  // Display the running processes of a container
